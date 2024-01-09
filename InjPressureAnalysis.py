9abc7bd3367600b520c6f4f8f1ccf7a0f7e4e2cb
Costaki Skevofilax <cskevofilax@utexas.edu>
2024-01-09 17:08:30 -0600

    added hversine formula to claculate distance between wells and earthquake

--- InjPressureAnalysis.py
+++ InjPressureAnalysis.py
@@ -1,8 +1,32 @@
 import pandas as pd
+from math import radians, sin, cos, sqrt, atan2
 
 # Paths to datasets
 injection_data_file_path = '/home/skevofilaxc/Downloads/injectiondata1624.csv'
 earthquake_data_file_path = '/home/skevofilaxc/PycharmProjects/earthquake-analysis/texnet_events-Mentone-20231128.csv'
+
+
+def haversine_distance(lat1, lon1, lat2, lon2):
+    # Earth radius in kilometers
+    R = 6371.0
+
+    # Convert latitude and longitude from degrees to radians
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    # Calculate differences in coordinates
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    # Haversine formula to calculate distance
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    # Calculate the distance
+    distance = R * c
+    return distance
 
 
 def extract_and_sort_data(csv_file):
