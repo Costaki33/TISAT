b9080e78c351a4270efe7d9bb4168fffaa3783a0
Costaki Skevofilax <cskevofilax@utexas.edu>
2024-01-09 16:56:03 -0600

    Fixed repo and script now pulls first earthquake data

--- InjPressureAnalysis.py
+++ InjPressureAnalysis.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Paths to datasets
-injection_data_file_path = r'C:\Users\costa\OneDrive\Documents\BEG\Wells With Injection Data 1.4.24\UICN000012239.csv'
-earthquake_data_file_path = r'C:\Users\costa\PycharmProjects\BEG\take2\texnet_events-Mentone-20231128.csv'
+injection_data_file_path = '/home/skevofilaxc/Downloads/injectiondata1624.csv'
+earthquake_data_file_path = '/home/skevofilaxc/PycharmProjects/earthquake-analysis/texnet_events-Mentone-20231128.csv'
 
 
 def extract_and_sort_data(csv_file):
@@ -44,6 +44,25 @@ def extract_columns(csv_file):
         return None
 
 
+def first_quake(data_frame):
+    """
+    Extracts information about the first earthquake in the given DataFrame.
+    """
+    try:
+        first_earthquake = data_frame.iloc[0]  # Extracting the first earthquake
+        first_earthquake_latitude = first_earthquake['Latitude (WGS84)']
+        first_earthquake_longitude = first_earthquake['Longitude (WGS84)']
+        first_earthquake_origin_date = first_earthquake['Origin Date']
+
+        return {
+            'Latitude': first_earthquake_latitude,
+            'Longitude': first_earthquake_longitude,
+            'Origin Date': first_earthquake_origin_date
+        }
+    except IndexError:
+        print("No earthquake data available.")
+        return None
+
 # Extracting and displaying well injection data
 extracted_injection_data = extract_columns(injection_data_file_path)
 if extracted_injection_data is not None:
@@ -54,3 +73,9 @@ if extracted_injection_data is not None:
 extracted_and_sorted_earthquake_data = extract_and_sort_data(earthquake_data_file_path)
 if extracted_and_sorted_earthquake_data is not None:
     print(extracted_and_sorted_earthquake_data.head())
+
+    # Using the first_quake function to get information about the first earthquake
+    first_quake_info = first_quake(extracted_and_sorted_earthquake_data)
+    if first_quake_info is not None:
+        print("Information about the first earthquake:")
+        print(first_quake_info)
