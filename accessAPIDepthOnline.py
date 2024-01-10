e80c139b3d04951f36c1147c290ec76247d8ed7d
Costaki Skevofilax <cskevofilax@utexas.edu>
2024-01-10 13:45:54 -0600

    finished script that pulls the depth of a given well provided the wells api number

--- accessAPIDepthOnline.py
+++ accessAPIDepthOnline.py
@@ -4,42 +4,45 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Replace with the path to your chromedriver executable
-service = Service('/usr/lib/chromium-browser/chromedriver')
-driver = webdriver.Chrome(service=service)
 
-# Open the website
-driver.get('https://webapps2.rrc.texas.gov/EWA/wellboreQueryAction.do')
+def get_api_depth(api_number):
+    service = Service('/usr/lib/chromium-browser/chromedriver')
+    driver = webdriver.Chrome(service=service)
 
-# Wait for the input fields to be present
-first_input_field = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.NAME, 'searchArgs.apiNoPrefixArg'))
-)
-second_input_field = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.NAME, 'searchArgs.apiNoSuffixArg'))
-)
+    # Open the website
+    driver.get('https://webapps2.rrc.texas.gov/EWA/wellboreQueryAction.do')
 
-# Split the API number
-api_number = '31741196'
-first_input = api_number[:3]  # '317'
-second_input = api_number[3:]  # '41196'
+    # Wait for the input fields to be present
+    first_input_field = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, 'searchArgs.apiNoPrefixArg'))
+    )
+    second_input_field = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, 'searchArgs.apiNoSuffixArg'))
+    )
 
-# Fill the input fields
-first_input_field.send_keys(first_input)
-second_input_field.send_keys(second_input)
+    # Split the API number
+    first_input = api_number[:3]  # '317'
+    second_input = api_number[3:]  # '41196'
 
-# Similarly, fill in other form fields using appropriate element identifiers
+    # Fill the input fields
+    first_input_field.send_keys(first_input)
+    second_input_field.send_keys(second_input)
 
-# Submit the form
-submit_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, '//input[@type="submit"]'))
-)
-submit_button.click()
+    # Similarly, fill in other form fields using appropriate element identifiers
 
-# Wait for the new page to load after submitting the form
-wait = WebDriverWait(driver, 10)
-api_depth_element = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/table[2]/tbody/tr/td/form/table/tbody/tr[2]/td/table/tbody/tr[2]/td/table/tbody/tr[3]/td[10]')))
+    # Submit the form
+    submit_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//input[@type="submit"]'))
+    )
+    submit_button.click()
 
-# Get the text of the API Depth element
-api_depth = api_depth_element.text
+    # Wait for the new page to load after submitting the form
+    wait = WebDriverWait(driver, 10)
+    api_depth_element = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/table[2]/tbody/tr/td/form/table/tbody/tr[2]/td/table/tbody/tr[2]/td/table/tbody/tr[3]/td[10]')))
 
+    # Get the text of the API Depth element
+    api_depth = api_depth_element.text
+
+    driver.quit()  # Close the browser window
+
+    return api_depth
